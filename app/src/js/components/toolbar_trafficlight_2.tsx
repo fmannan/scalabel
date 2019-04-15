dd733e20ba048557daec4e9332b59cb0d3d48bff
Bohan Zhai <zhaibohan@berkeley.edu>
2019-04-15 03:09:41 -0700

    need to fix the fontsize

--- app/src/js/components/toolbar_trafficlight_2.tsx
+++ app/src/js/components/toolbar_trafficlight_2.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 import {Validator} from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-// @ts-ignore
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
 const styles = (theme: { spacing: { unit: number; }; palette: { background: { default: any; }; }; }) => ({
+    root: {
+        height: '25px',
+        width: '25px',
+        fontSize: '12px'
+    },
     toggleContainer: {
-        height: 56,
-        padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+        height: 40,
+        width: 250,
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        margin: `${theme.spacing.unit}px 0`,
-        background: theme.palette.background.default
+        background: 'rgba(250,250,250,0)'
+    },
+    buttonGroup: {
+        height: '25px',
+        width: '200px'
+    },
+    label: {
+        fontSize: '12px'
     }
 });
 
@@ -33,25 +43,25 @@ class ToggleButtons extends React.Component<Props> {
     public static propTypes: { classes: Validator<NonNullable<object>> };
 
     public render() {
-
-        const { classes } = this.props;
         const { name } = this.props;
+        const { classes } = this.props;
         const { alignment} = this.state;
 
         return (
             <Grid container spacing={16}>
                 <Grid item xs={12} sm={6}>
-                    <div> {name} </div>
+                    <div className = {classes.string}> {name} </div>
                     <div className={classes.toggleContainer}>
                         <ToggleButtonGroup
+                            className={classes.buttonGroup}
                             value={alignment}
-                        exclusive
-                        onChange={this.handleAlignment}
+                            exclusive
+                            onChange={this.handleAlignment}
                         >
-                            <ToggleButton value='left'>N/A</ToggleButton>
-                            <ToggleButton value='green'>G</ToggleButton>
-                            <ToggleButton value='yellow'>Y</ToggleButton>
-                            <ToggleButton value='red'>R</ToggleButton>
+                            <ToggleButton  value = 'left'> N/A </ToggleButton>
+                            <ToggleButton  value = 'green'> G </ToggleButton>
+                            <ToggleButton  value = 'yellow'> Y </ToggleButton>
+                            <ToggleButton  value = 'red'> R </ToggleButton>
                         </ToggleButtonGroup>
                     </div>
                 </Grid>
